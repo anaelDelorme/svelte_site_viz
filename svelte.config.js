241ca9654d0599744028f39fdcdbc0bbed67a2df
Anaël Delorme <87323681+anaelDelorme@users.noreply.github.com>
2024-02-29 15:55:14 +0000

    init in docs

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import mdsvex from "mdsvex"
 import sveltePreprocess from "svelte-preprocess"
-import adapterStatic from "@sveltejs/adapter-static"
+import adapter from "@sveltejs/adapter-static"
 import mdsvexConfig from "./mdsvex.config.cjs"
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -16,7 +16,15 @@ export default {
 	],
 
 	kit: {
-		adapter: adapterStatic(),
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: "docs",
+			assets: "docs",
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
 		paths: {
 			base: process.env.BASE_URL || "",
 		},
